4c53c3c390bab0ffb8f162c5d9e79ba9a01ca8b4
Vidar Hokstad <vidar@hokstad.com>
2009-04-23 13:08:22 -0400

    Slightly improved appearance

--- lib/sliding-stats/view.rb
+++ lib/sliding-stats/view.rb
@@ -7,9 +7,29 @@ module SlidingStats
   # view by subclassing and overriding the #show method, or replacing it
   # completely.
   class View
+    FOOTER = <<-end_footer
+    </table>
+    <div style='margin-top: 50px'>Stats by <a href='http://www.hokstad.com/slidingstats'>Sliding Stats</a> -- Copyright 2009 <a href='http://www.hokstad.com/'>Vidar Hokstad</a>. </div>
+    </body></html>
+    end_footer
+
+    CSS = <<-end_css
+      h1, h2 { font-family: 'Lucida Sans Unicode', 'Lucida Grande', sans-serif; }
+          h2 { margin-top: 20px;}
+
+      table  { display: inline; margin-top: 20px; margin-left: 100px; width: 90%; border: outset 1px grey; 
+               background: #aaaaff; padding: 0px; align: left; text-align: left;
+             }
+      table.breakdown { background: #ccccff; margin-top: 1px; width: 100%; margin-left: 0px; padding: 5px; }
+      table.breakdown  td.count { width: 40px; }
+      td.name { width: 50%; }
+      tr.odd { background: #aaaaff; }
+      tr.even { background: #bbbbff; }
+    end_css
+
     def show(data)
       r = Rack::Response.new
-      r.write("<html><head><title>Sliding Stats</title><style>h2 {margin-top: 20px;}</style> <body>")
+      r.write("<html><head><title>Sliding Stats</title><style>" + CSS + "</style> <body>")
       r.write("<h1>Sliding Stats</h1>")
       # Setting the size here is a *hack*. Need to fix that
       r.write("<h2>Most recent referrers</h2>")
@@ -17,21 +37,22 @@ module SlidingStats
       r.write("<h2>Most recent pages</h2>")
       r.write("<div style='width: 1000px;'><embed pluginspage=\"http://www.adobe.com/svg/viewer/install/\" type=\"image/svg+xml\" src=\"#{data[:base]}/pages.svg\" style=\"margin-left: 50px; width: 1000px; height: #{40 + 20*data[:pages].size}px;\"></div>")
       r.write("<h2>Most recent referrers broken down by pages</h2>")
-      r.write("<table border='1' style=\"display:inline; margin-top: 20px; margin-left: 100px; width: 950px; \"><tr><th>Referer</th><th>Pages</th></tr>\n")
+      r.write("<table><tr><th>Referer</th><th>Pages</th></tr>\n")
+      odd = true
       data[:referers_to_pages].each do |k,v|
-        r.write("<tr><td>#{CGI.escapeHTML(k)}</td> <td><table>")
+        k = k[0..79] + "..." if k.length > 80
+        r.write("<tr class='#{odd ? 'odd':'even'}'><td class='name'>#{CGI.escapeHTML(k)}</td> <td><table class='breakdown'>")
         total = v[:total]
         if v.size > 2 # include :total
-          r.write("<tr><td>#{total}</td><td><strong>total</strong></td></tr>")
+          r.write("<tr><td class='count'>#{total}</td><td><strong>total</strong></td></tr>")
         end
         v.sort_by{|page,count| -count}.each do |page,count| 
-          r.write("<tr><td>#{count}</td><td>#{page.to_s}</td></tr>") if page != :total
+          r.write("<tr><td class='count'>#{count}</td><td>#{page.to_s}</td></tr>") if page != :total
         end
         r.write("</table></td></tr>\n")
+        odd = !odd
       end
-      r.write("</table>")
-      r.write("<div style='margin-top: 50px'>Stats by <a href='http://www.hokstad.com/slidingstats'>Sliding Stats</a> -- Copyright 2009 <a href='http://www.hokstad.com/'>Vidar Hokstad</a>. </div>")
-      r.write("</body></html>")
+      r.write(FOOTER)
       r.finish
     end
 
